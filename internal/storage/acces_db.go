4af4cc16dc11d9a59622ced0d3d653855ddbb605
Matthew3000 <kruglikov.matvey3000@gmail.com>
2022-10-17 16:43:57 +0300

    Update acces_db.go

--- internal/storage/acces_db.go
+++ internal/storage/acces_db.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"gophermart/internal/service"
+	"io"
 	"log"
 	"net/http"
 )
@@ -73,12 +74,8 @@ func (dbStorage DBStorage) GetOrderStatus(order service.Order, accrualAddr strin
 	}
 	defer response.Body.Close()
 
-	//value, err := io.ReadAll(response.Body)
-	//if err != nil {
-	//	log.Printf("read response body err: %s", err)
-	//	return order, err
-	//}
-	//log.Printf("response body is  %s", value)
+	value, _ := io.ReadAll(response.Body)
+	log.Printf("response body is  %s", value)
 
 	var orderResponse service.OrderAccrualResponse
 	if err := json.NewDecoder(response.Body).Decode(&orderResponse); err != nil {
